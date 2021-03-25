0fef46d41f776d2022be6f8c052e9b4398b0de8f
mooreFL <moorepr09@gmail.com>
2021-03-25 16:13:42 -0400

    added if functions alerts and returns

--- script.js
+++ script.js
@@ -1,17 +1,89 @@
-var SpecialCharacters = ['@','!','#','$','%','^','&','*','<','?','+','~','_','-','/','|','{','}','[',']'];
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'];
-var uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var SpecialCharacters = [
+  "@",
+  "!",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "<",
+  "?",
+  "+",
+  "~",
+  "_",
+  "-",
+  "/",
+  "|",
+  "{",
+  "}",
+  "[",
+  "]",
+];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercaseCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercaseCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 // final array for password
-var finalPass = [];
-
 
 // Assignment Code
-characterSet.push(SpecialCharacters,numericCharacters,lowercaseCharacters,uppercaseCharacters);
-console.log(characterSet);
+// characterSet.push(SpecialCharacters,numericCharacters,lowercaseCharacters,uppercaseCharacters);
+// console.log(characterSet);
 
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
@@ -19,10 +91,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Math.random(); 
+// Math.random();
 Math.random();
 
 // Add event listener to generate button
@@ -30,41 +101,54 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // write and determing what my password should be
+  var randomPassword = [];
+  var finalPass = [];
 
-  var passwordLength = prompt("Enter the length of your password");
+  var passwordLength = parseInt(prompt("Enter the length of your password"));
+// password length is less than 8 or greater than 128 do not run the functions 
+if (passwordLength < 8) {
+    // return message
+    alert("Please select length between 8 and 128 characters")
+    return
+  }
+  if (passwordLength > 128) {
+    // return message
+    alert("Please select length between 8 and 128 characters")
+    return
+  }
 
   // if statement to determine lenght is between 8-128 characters
   var hasNumbers = confirm("Include Numbers?");
-  var haslowerCase = confirm ("Include lowercase?")
+  var haslowerCase = confirm("Include lowercase?");
   var hasspecialCharacters = confirm("Include special characters?");
-  var hasupperCase = confirm ("Include uppercase?");
+  var hasupperCase = confirm("Include uppercase?");
 
-if (hasNumbers) {
-  finalPass.concat(SpecialCharacters);
-}
+  if (hasNumbers) {
+    finalPass = finalPass.concat(numericCharacters);
+  }
 
-console.log(finalPass);
+  if (haslowerCase) {
+    finalPass = finalPass.concat(lowercaseCharacters);
+  }
 
-if (haslowerCase) {
-  finalPass.concat(haslowerCase);
-}
+  if (hasspecialCharacters) {
+    finalPass = finalPass.concat(SpecialCharacters);
+  }
 
-console.log(finalPass);
+  if (hasupperCase) {
+    finalPass = finalPass.concat(uppercaseCharacters);
+  }
 
-if (hasspecialCharacters){
-  finalPass.concat(hasspecialCharacters);
-}
+  for (var i = 0; i < passwordLength; i++) {
+    console.log(finalPass);
+    var randomIndex = Math.floor(Math.random() * finalPass.length);
+    console.log(randomIndex);
+    var randomCharacter = finalPass[randomIndex];
+    console.log(randomCharacter);
+    randomPassword.push(randomCharacter);
+    console.log(randomPassword)
+  }
 
-if (hasupperCase){
-  finalPass.concat(hasupperCase);
-}
-
-
-
-  return finalPass
-
-}
-
-else {
-  alert("Password must be between 8 and 128 characters")
+  return randomPassword.join("");
+  
 }
